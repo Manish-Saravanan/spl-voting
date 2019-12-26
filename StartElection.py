2f74e717737ab15c449fadf908b3cdd656ff91e5
Manish-Saravanan <manish.rathika@gmail.com>
2019-12-26 11:02:02 +0530

    A complete cycle from login to end of voting established in the user interface.

--- StartElection.py
+++ StartElection.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-data = [["SPL-Girl", [["candidate1", "path1", "no of votes"],["candidate2", "path2", "no of votes"],["candidate3", "path3", "no of votes"],["candidate4", "path4", "no of votes"]]],
-        ["SPL-Boy", [["candidate1", "path1", "no of votes"],["candidate2", "path2", "no of votes"],["candidate3", "path3", "no of votes"],["candidate4", "path4", "no of votes"]]],
-        ["ASPL-Boy", [["candidate1", "path1", "no of votes"],["candidate2", "path2", "no of votes"],["candidate3", "path3", "no of votes"],["candidate4", "path4", "no of votes"]]],
-        ["ASPL-Girl", [["candidate1", "path1", "no of votes"],["candidate2", "path2", "no of votes"],["candidate3", "path3", "no of votes"],["candidate4", "path4", "no of votes"]]]]
+data = [["SPL-Girl", [["candidate1", "path1.jpg", "no of votes"],["candidate2", "path2.jpg", "no of votes"],["candidate3", "path3.jpg", "no of votes"],["candidate4", "path4.jpg", "no of votes"],["candidate5", "path2.jpg", "no of votes"]]],
+        ["SPL-Boy", [["candidate1", "path1.jpg", "no of votes"],["candidate2", "path2.jpg", "no of votes"],["candidate3", "path3.jpg", "no of votes"],["candidate4", "path4.jpg", "no of votes"]]],
+        ["ASPL-Boy", [["candidate1", "path1.jpg", "no of votes"],["candidate2", "path2.jpg", "no of votes"],["candidate3", "path3.jpg", "no of votes"],["candidate4", "path4.jpg", "no of votes"]]],
+        ["ASPL-Girl", [["candidate1", "path1.jpg", "no of votes"],["candidate2", "path2.jpg", "no of votes"],["candidate3", "path3.jpg", "no of votes"],["candidate4", "path4.jpg", "no of votes"]]]]
+
+credentials = {'123456':"54321", '567890':"09876"}
+
 
 
 @app.route('/login')
-def hello():
+def login_view():
     return render_template("login.html")
 
 @app.route('/vote')
@@ -18,14 +21,21 @@ def voting():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    print("Hello")
+    votes = request.form.getlist("SPL-Girl")
     print(request.form)
-    return "Thank you."
-    sleep(120)
     return render_template("login.html")
 
-
-
+@app.route('/authenticate', methods=['POST'])
+def auth_task():
+    usnnum = request.form.getlist('usnno')
+    passcode = request.form.getlist('pass')
+    if usnnum[0] in credentials:
+        if credentials[usnnum[0]]==passcode[0]:
+            return render_template("Vote-page.html", data = data)
+        else:
+            return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 if __name__ == '__main__':
     app.run()
